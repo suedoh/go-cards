749e6de8f2d5eecc5b529ce1b7b2a0d00aa1f891
suedoh <suedoh36@proton.me>
2023-03-03 21:08:11 -0500

    Using deck type, receiver and function from deck.go

--- main.go
+++ main.go
@@ -1,20 +1,8 @@
 package main
 
-import "fmt"
-
 func main() {
-    cards := []string{"Ace of Diamonds", newCard()}
-    cards = append(cards, "Six of Spades")
+    cards := newDeck()
 
-    for i, card := range cards {
-        fmt.Println(i, card)
-        fmt.Println(card[i])
-        fmt.Println(card)
-    }
-
-    fmt.Println(cards)
+    cards.print()
 }
 
-func newCard() string {
-   return "Five of Diamonds" 
-}
