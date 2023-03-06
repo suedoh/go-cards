77d1a7bb0f845fec0432e09d1d58ac5ea8281863
suedoh <suedoh36@proton.me>
2023-03-05 23:01:31 -0500

    changed main to create deck, shuffle cards and print hand out

--- main.go
+++ main.go
@@ -2,10 +2,7 @@ package main
 
 func main() {
     cards := newDeck()
-
-    hand, remainingCards := deal(cards, 5)
-    
-    hand.print()
-    remainingCards.print()
+    cards.shuffle()
+    cards.print()
 }
 
