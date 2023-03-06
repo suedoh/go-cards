2e105ba447034e5c1b8ddf75d1156a0193847bbb
suedoh <suedoh36@proton.me>
2023-03-05 22:52:01 -0500

    Completed tests for creating deck, saving to file and reading file

--- deck_test.go
+++ deck_test.go
@@ -1,11 +1,41 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T)  {
     d := newDeck()
-    
+    firstCard := "Ace of Spades"
+    lastCard := "Four of Clubs"
+
     if len(d) != 16 {
         t.Errorf("Expected deck length of 16 but got %v", len(d))
     }
+
+    if d[0] != firstCard {
+        t.Errorf("Expected first card to be %v but got %v", firstCard, d[0])
+    }
+
+    if d[len(d) - 1] != lastCard {
+        t.Errorf("Expected last card to be %v but got %v", lastCard, d[len(d) - 1])
+    }
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T)()  {
+    testFile := "_decktesting"
+
+    os.Remove(testFile)
+
+    deck := newDeck()
+    deck.saveToFile(testFile)
+
+    loadedDeck := newDeckFromFile(testFile)
+
+    if len(loadedDeck) != 16 {
+        t.Errorf("Expected 16 cards but received %v", len(loadedDeck))
+    }
+
+    os.Remove(testFile)
 }
